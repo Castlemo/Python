5c17b9e347029483f3f2086b44e777c1a671c3eb
Castlemo <yeonsm0616@naver.com>
2025-07-15 16:26:22 +0900

    11장 모듈과 패키지

---
+++ Ch11_모듈과_패키지/byme.py
@@ -0,0 +1,9 @@
+from IPython.display import HTML, display
+
+def sign():
+    print("이 프로그램은 [리자몽]이 만들었습니다.")
+    print("이메일 : yeonsm0616@naver.com")
+    print("학교 : 한성대학교 비즈니스 애널리틱스 / 빅데이터 트랙")
+    display(HTML('<a href="https://pokemondb.net/pokedex/charizard">'
+                '<img src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" '
+                'alt="Charizard"></a>'))
